5e7277123c26155941a8566a6a0de6576de53e2a
Yelnur Abdrakhmanov <79540438+prenl@users.noreply.github.com>
2024-03-04 19:54:31 +0500

    feat: Add user model timestamps

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,5 +1,9 @@
 package models
 
+import (
+	"time"
+)
+
 type User struct {
 	ID             int
 	Username       string
@@ -7,4 +11,18 @@ type User struct {
 	Email          string
 	Phone          string
 	IsAdmin        bool
+	CreatedAt      time.Time
+	UpdatedAt 	   time.Time
+}
+
+func NewUser(username, hashedPassword, email, phone string, isAdmin bool, createdAt, updatedAt time.Time) *User {
+	return &User{
+		Username:       username,
+		HashedPassword: hashedPassword,
+		Email:          email,
+		Phone:          phone,
+		IsAdmin:        isAdmin,
+		CreatedAt:      createdAt,
+		UpdatedAt:      updatedAt,
+	}
 }
