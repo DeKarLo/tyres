8a81989ca79ffc8b49cc343fabc271a1f2de2cdb
DeKarLo <karen.ananyan2017@mail.ru>
2024-03-04 21:56:42 +0600

    post repository

--- internal/models/post.go
+++ internal/models/post.go
@@ -9,16 +9,18 @@ type Post struct {
 	Title     string
 	Content   string
 	Img       string
+	Price     int
 	CreatedAt time.Time
-	UpdatetAt time.Time
+	UpdatedAt time.Time
 }
 
-func (p *Post) NewPost(title, content, img string, createdAt, updatedAt time.Time) *Post {
+func (p *Post) NewPost(title, content, img string, price int, createdAt, updatedAt time.Time) *Post {
 	return &Post{
 		Title:     title,
 		Content:   content,
 		Img:       img,
+		Price:     price,
 		CreatedAt: createdAt,
-		UpdatetAt: updatedAt,
+		UpdatedAt: updatedAt,
 	}
 }
---
+++ internal/repositories/post_repository.go
@@ -0,0 +1,121 @@
+package repositories
+
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+	"tyres.kz/internal/models"
+)
+
+type PostRepositoryInterface interface {
+	Create(post *models.Post) error
+	GetAll() ([]*models.Post, error)
+	GetByID(id int) (*models.Post, error)
+	GetByName(name string) (*models.Post, error)
+	Update(post *models.Post) error
+	Delete(id int) error
+}
+
+type PostRepository struct {
+	db     *sql.DB
+	logger *log.Logger
+}
+
+func CreatePostRepository(db *sql.DB, logger *log.Logger) *PostRepository {
+	return &PostRepository{db, logger}
+}
+
+func (repository *PostRepository) Create(post *models.Post) error {
+	if post == nil {
+		repository.logger.Println("Attempted to create nil value post")
+		return nil
+	}
+
+	query := "insert into posts (name, description, price, created_at, updated_at) values (?, ?, ?, ?, ?)"
+	_, err := repository.db.Exec(query, post.Title, post.Content, post.Price, post.CreatedAt, post.UpdatedAt)
+
+	if err != nil {
+		repository.logger.Printf("Error inserting post: %v", err)
+		return err
+	}
+
+	return nil
+}
+
+func (repository *PostRepository) GetAll() ([]*models.Post, error) {
+	query := "select * from posts"
+	rows, err := repository.db.Query(query)
+
+	if err != nil {
+		repository.logger.Printf("Error getting all posts: %v", err)
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	posts := make([]*models.Post, 0)
+	for rows.Next() {
+		post := models.Post{}
+		err := rows.Scan(&post.ID, &post.Title, &post.Content, &post.Price, &post.CreatedAt, &post.UpdatedAt)
+		if err != nil {
+			repository.logger.Printf("Error scanning post: %v", err)
+			return nil, err
+		}
+		posts = append(posts, &post)
+	}
+
+	return posts, nil
+}
+
+func (repository *PostRepository) GetByID(id int) (*models.Post, error) {
+	query := "select * from posts where id = ?"
+	row := repository.db.QueryRow(query, id)
+
+	post := models.Post{}
+	err := row.Scan(&post.ID, &post.Title, &post.Content, &post.Price, &post.CreatedAt, &post.UpdatedAt)
+	if err != nil {
+		repository.logger.Printf("Error scanning post: %v", err)
+		return nil, err
+	}
+
+	return &post, nil
+}
+
+func (repository *PostRepository) GetByName(name string) (*models.Post, error) {
+	query := "select * from posts where name = ?"
+	row := repository.db.QueryRow(query, name)
+
+	post := models.Post{}
+	err := row.Scan(&post.ID, &post.Title, &post.Content, &post.Price, &post.CreatedAt, &post.UpdatedAt)
+	if err != nil {
+		repository.logger.Printf("Error scanning post: %v", err)
+		return nil, err
+	}
+
+	return &post, nil
+}
+
+func (repository *PostRepository) Update(post *models.Post) error {
+	query := "update posts set name = ?, description = ?, price = ?, updated_at = ? where id = ?"
+	_, err := repository.db.Exec(query, post.Title, post.Content, post.Price, post.UpdatedAt, post.ID)
+
+	if err != nil {
+		repository.logger.Printf("Error updating post: %v", err)
+		return err
+	}
+
+	return nil
+}
+
+func (repository *PostRepository) Delete(id int) error {
+	query := "delete from posts where id = ?"
+	_, err := repository.db.Exec(query, id)
+
+	if err != nil {
+		repository.logger.Printf("Error deleting post: %v", err)
+		return err
+	}
+
+	return nil
+}
