2c34220a65a2b048ae3dd510d5ee165de142eb82
Yelnur Abdrakhmanov <79540438+prenl@users.noreply.github.com>
2024-03-06 20:50:16 +0500

    fix: add services to handlers

--- cmd/web/handlers/post_handler.go
+++ cmd/web/handlers/post_handler.go
@@ -5,40 +5,66 @@ import (
 	"log"
 	"net/http"
 	"path/filepath"
+	"strconv"
+
+	"tyres.kz/internal/services"
 )
 
-func CreatePostPage(w http.ResponseWriter, r *http.Request) {
+type PostHandler struct {
+	postService *services.PostService
+	userService *services.UserService
+	logger      *log.Logger
+}
+
+func (postHandler *PostHandler) CreatePostPage(w http.ResponseWriter, r *http.Request) {
 	tmpl, err := template.ParseFiles(
 		filepath.Join("cmd/web/ui/views/pages", "create-post.tmpl.html"),
 		filepath.Join("cmd/web/ui/views", "base-100vh.tmpl.html"))
 
 	if err != nil {
-		log.Println(err)
+		postHandler.logger.Println(err)
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
 
 	err = tmpl.Execute(w, nil)
 	if err != nil {
-		log.Println(err)
+		postHandler.logger.Println(err)
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
 }
 
-func GetPost(w http.ResponseWriter, r *http.Request) {
+func (postHandler *PostHandler) GetPost(w http.ResponseWriter, r *http.Request) {
 	return
 }
 
-func CreatePost(w http.ResponseWriter, r *http.Request) {
+func (postHandler *PostHandler) CreatePost(w http.ResponseWriter, r *http.Request) {
 	return
 }
 
-func GetAllPosts(w http.ResponseWriter, r *http.Request) {
+func (postHandler *PostHandler) GetAllPosts(w http.ResponseWriter, r *http.Request) {
 	return
 }
 
-func UpdatePostPage(w http.ResponseWriter, r *http.Request) {
+func (postHandler *PostHandler) UpdatePostPage(w http.ResponseWriter, r *http.Request) {
+	postId := r.URL.Query().Get("id")
+	postIdInt, err := strconv.Atoi(postId)
+
+	if err != nil {
+		postHandler.logger.Println(err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	post, err := postHandler.postService.GetPostByID(postIdInt)
+
+	if err != nil {
+		postHandler.logger.Println(err)
+		http.Error(w, "Post was not found", http.StatusNotFound)
+		return
+	}
+
 	tmpl, err := template.ParseFiles(
 		filepath.Join("cmd/web/ui/views/pages", "update-post.tmpl.html"),
 		filepath.Join("cmd/web/ui/views", "base-100vh.tmpl.html"))
@@ -49,7 +75,7 @@ func UpdatePostPage(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	err = tmpl.Execute(w, nil)
+	err = tmpl.Execute(w, map[string]interface{}{"post": post})
 	if err != nil {
 		log.Println(err)
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
@@ -57,10 +83,10 @@ func UpdatePostPage(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func UpdatePost(w http.ResponseWriter, r *http.Request) {
+func (postHandler *PostHandler) UpdatePost(w http.ResponseWriter, r *http.Request) {
 	return
 }
 
-func DeletePost(w http.ResponseWriter, r *http.Request) {
+func (postHandler *PostHandler) DeletePost(w http.ResponseWriter, r *http.Request) {
 	return
 }
