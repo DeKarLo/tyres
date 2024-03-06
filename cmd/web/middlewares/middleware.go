6fcd04aa3c0dd2b0ea5e8b80f512f2c9d1819717
Qanysh <220667@astanait.edu.kz>
2024-03-06 18:46:57 +0500

    update middleware

--- cmd/web/middlewares/middleware.go
+++ cmd/web/middlewares/middleware.go
@@ -4,24 +4,26 @@ import (
 	"net/http"
 )
 
+// check authentification
 func ensureAuthenticated(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.Header.Get("userId") == "" {
 			http.Error(w, "Unauthorized", http.StatusUnauthorized)
 			return
 		}
-		
+
 		next.ServeHTTP(w, r)
 	})
 }
 
+// if not authentificated
 func ensureNotAuthenticated(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.Header.Get("userId") != "" {
 			http.Error(w, "Unauthorized", http.StatusUnauthorized)
 			return
 		}
-		
+
 		next.ServeHTTP(w, r)
 	})
 }
