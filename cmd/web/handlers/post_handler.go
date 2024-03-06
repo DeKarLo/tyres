d1a8bfa03a06e118517837c16dc433a794ceae8f
DeKarLo <karen.ananyan2017@mail.ru>
2024-03-06 21:17:36 +0600

    add: create-post, update post pages handler

--- cmd/web/handlers/post_handler.go
+++ cmd/web/handlers/post_handler.go
@@ -1,11 +1,29 @@
 package handlers
 
 import (
+	"html/template"
+	"log"
 	"net/http"
+	"path/filepath"
 )
 
 func CreatePostPage(w http.ResponseWriter, r *http.Request) {
-	return
+	tmpl, err := template.ParseFiles(
+		filepath.Join("cmd/web/ui/views/pages", "create-post.tmpl.html"),
+		filepath.Join("cmd/web/ui/views", "base-100vh.tmpl.html"))
+
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	err = tmpl.Execute(w, nil)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
 }
 
 func GetPost(w http.ResponseWriter, r *http.Request) {
@@ -18,6 +36,25 @@ func CreatePost(w http.ResponseWriter, r *http.Request) {
 
 func GetAllPosts(w http.ResponseWriter, r *http.Request) {
 	return
+}
+
+func UpdatePostPage(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles(
+		filepath.Join("cmd/web/ui/views/pages", "update-post.tmpl.html"),
+		filepath.Join("cmd/web/ui/views", "base-100vh.tmpl.html"))
+
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	err = tmpl.Execute(w, nil)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
 }
 
 func UpdatePost(w http.ResponseWriter, r *http.Request) {
